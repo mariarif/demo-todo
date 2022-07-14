22de054ffc1ed08b861ebc2f73c02277867e49cd
mariarif <ginamaria.rif@gmail.com>
2022-07-14 15:23:09 +0300

    commit

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,21 +3,28 @@ import TodoListItem from './TodoListItem.vue';
 
 const response = await fetch('https://jsonplaceholder.typicode.com/todos');
 const todos = await response.json();
-const nameVar = "def";
 </script>
 
 <template>
-    <ul>
+    <div class="div-item" >
         <TodoListItem
             v-for="todo of todos"
             :key="todo.id"
+            :userId="todo.userId"
             :id="todo.id"
             :title="todo.title"
             :completed="todo.completed"
         />
-    </ul>
+    </div>
 </template>
 
 <style scoped>
-
+.div-item{
+    border: 4px dotted rgb(18, 61, 9) ;
+    margin: 20px;
+    padding: 15px;
+    display: grid;
+    grid-template-columns: 10% 70% 20%;
+    background-color: rgb(148, 187, 140);
+}
 </style>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,5 +1,6 @@
 <script setup>
 defineProps({
+    userId: Number,
     id: Number,
     title: String,
     completed: Boolean
@@ -7,10 +8,46 @@ defineProps({
 </script>
 
 <template>
-    <input type="checkbox" :checked="completed" />
-    <p>{{ title }}</p>
-    <button>Delete</button>
+    <!-- <div class="grid-container">
+        <div class="grid-line">    
+            <input type="checkbox" :checked="completed" />
+            {{ title }}
+            <button>Delete</button>
+        </div> 
+    </div> -->
+
+    <div class="grid-item">
+        <input type="checkbox" :checked="completed" />
+    </div>
+    <div class="grid-item">
+        {{ title }}
+    </div>
+    <div class="grid-item">
+        <button class="btnDelete">Delete</button>
+    </div>
+
 </template>
 
 <style scoped>
+/* .grid-container{
+    display: grid;
+    background-color: rgb(175, 216, 175);
+    
+}
+.grid-line{
+    margin: 10px;
+    padding: 15px;
+    display: inline-grid;
+    background-color: rgb(222, 240, 212);
+} */
+.grid-item{
+    border: 2px groove rgb(143, 160, 134) ;
+    background-color: rgb(222, 240, 212);
+    margin: 5px;
+    padding: 10px;
+}
+.btnDelete{
+    width: 100%;
+    background-color: rgb(148, 187, 140);
+}
 </style>
